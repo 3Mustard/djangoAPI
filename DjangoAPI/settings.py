b8a68992cd6df064b6dcf012caac67bfb94a5006
Matt <m23.cullen@gmail.com>
2021-05-12 13:23:20 -0400

    added new app AlbumApp

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -40,8 +40,10 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleWare',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
