a7295b1f3937eb5dea806e3b2d2e6b82ce496919
Matt <m23.cullen@gmail.com>
2021-05-12 16:47:31 -0400

    create view/urlPatterns for saving a file

--- AlbumApp/urls.py
+++ AlbumApp/urls.py
@@ -1,9 +1,16 @@
+from django.conf import urls
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
+
 from AlbumApp import views
 
 urlpatterns=[
     url(r'^group/$',views.groupApi),
     url(r'^group/([0-9]+)$',views.groupApi),
+
     url(r'^album/$',views.albumApi),
-    url(r'^album/([0-9]+)$',views.albumApi)
-]
+    url(r'^album/([0-9]+)$',views.albumApi),
+
+    url(r'^saveFile$', views.saveFile)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- AlbumApp/views.py
+++ AlbumApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
+from django.core.files.storage import default_storage
 
 from AlbumApp.models import Albums,Groups
 from AlbumApp.serializers import AlbumSerializer,GroupSerializer
@@ -67,3 +68,10 @@ def albumApi(request,id=0):
         album = Albums.objects.get(AlbumId=id)
         album.delete()
         return JsonResponse("Deleted album Successfully", safe=False)
+
+@csrf_exempt
+def saveFile(request):
+    file = request.FILES['uploadedFile']
+    file_name = default_storage.save(file.name,file)
+
+    return JsonResponse(file_name, safe=False)
