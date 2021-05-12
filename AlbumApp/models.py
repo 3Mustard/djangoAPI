362aafe0274222738c0e4e7a6b149a84cae970b8
Matt <m23.cullen@gmail.com>
2021-05-12 13:37:35 -0400

    created Models for Groups and Albums and ran makemigration

--- AlbumApp/models.py
+++ AlbumApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Groups(models.Model):
+    GroupId = models.AutoField(primary_key=True)
+    GroupName = models.CharField(max_length=20)
+    PhotoFileName = models.CharField(max_length=100)
+
+
+class Albums(models.Model):
+    AlbumId = models.AutoField(primary_key=True)
+    AlbumName = models.CharField(max_length=25)
+    Group = models.CharField(max_length=25)
+    DateAdded = models.DateField()
+    PhotoFileName = models.CharField(max_length=100)
