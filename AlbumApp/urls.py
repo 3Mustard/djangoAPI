8838827f4d3ac80a47642e75ce5cddab5f1a419c
Matt <m23.cullen@gmail.com>
2021-05-12 16:33:35 -0400

    added url patterns for albumApi

--- AlbumApp/urls.py
+++ AlbumApp/urls.py
@@ -3,5 +3,7 @@ from AlbumApp import views
 
 urlpatterns=[
     url(r'^group/$',views.groupApi),
-    url(r'^group/([0-9]+)$',views.groupApi)
+    url(r'^group/([0-9]+)$',views.groupApi),
+    url(r'^album/$',views.albumApi),
+    url(r'^album/([0-9]+)$',views.albumApi)
 ]
