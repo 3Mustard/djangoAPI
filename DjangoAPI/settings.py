4d11513449c19e1ed41f23e85a08c3e378dd74c5
Matt <m23.cullen@gmail.com>
2021-05-12 13:25:55 -0400

    add rest framework and albumapp to project installed apps

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework',
+    'AlbumApp.apps.AlbumappConfig',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
